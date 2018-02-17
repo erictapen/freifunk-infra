6939675786d71d78ff4a2e8ca9d8f06ddbcff19f
Justin Humm <justin.humm@posteo.de>
2018-02-17 20:44:13 +0100

    erste funktionierende batman config, die mit einem FFS-Router mesht

--- batman/default.nix
+++ batman/default.nix
@@ -6,5 +6,26 @@ let
 in
 
 mkIf cfg.enable {
-   services.postgresql.enable = true;
+  boot = { 
+    extraModulePackages = with config.boot.kernelPackages; [ batman_adv netatop ];
+    kernelModules = [ "batman_adv" ];
+  };
+
+  environment.systemPackages = with pkgs;[
+    batctl
+  ];
+
+  systemd.services = {
+    "batman-lan" = {
+      after = [ "network-interfaces.target" ];
+      wantedBy = [ "multi-user.target" ];
+      script = ''
+        # TODO enp4s5 kann später z.B. durch cfg.network.lan."ff-local".interface ersetzt werden 
+        ${pkgs.batctl}/bin/batctl -m "bat-lan" if add enp4s5
+      '';
+      serviceConfig.Type = "oneshot";
+    };
+    # "batman-wan" = {
+    # };
+  };
 }
