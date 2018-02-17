d1d560dc70d0cd0ed0203250b05f624fb980d524
0x4A6F <0x4A6F@users.noreply.github.com>
2018-02-17 18:36:15 +0000

    services.gateway.network: - introduce options

--- default.nix
+++ default.nix
@@ -1,9 +1,40 @@
-{ config, pkgs, lib, ...}:
+{ config, lib, pkgs, ...}:
 
 with lib;
 
 let
   cfg = config.services.gateway;
+  network = attrValues cfg.network;
+  networkOpts = { name, ... }: {
+
+    options = {
+
+      name = mkOption {
+        example = "wan-default";
+        type = types.str;
+        description = "Name of the network.";
+      };
+
+      interface = mkOption {
+        example = "eth0";
+        type = types.str;
+        description = "Name of the interface.";
+      };
+      protocol = mkOption {
+        example = "ip";
+        type = types.str;
+        description = "Name of the protocol.";
+      };
+      policy = mkOption {
+        example = "wan-dhcp";
+        type = types.str;
+        description = "Name of the policy.";
+      };
+    };
+    config = {
+      name = mkDefault name;
+    };
+  };
 in {
   imports = (mkIf cfg.enable { imports = [
     ./batman
@@ -15,8 +46,25 @@ in {
     services.gateway = {
       enable = mkEnableOption "the generic Gateway service";
     };
+
+    services.gateway.network = mkOption {
+      default = {};
+      example =
+        { wan-default = [ {
+            interface = "eth0";
+            policy = "wan-dhcp";
+            protocol = "ip";
+          } ];
+        };
+      description = ''
+        The configuration for each network on this gateway.
+        Layer3 (ip) or Layer2 (batman) Network protocols, soon also your protocol is supported, just a PR away.
+      '';
+      type = with types; loaOf (submodule networkOpts);
+    };
   };
 
   config = mkIf cfg.enable {
   };
+
 }
