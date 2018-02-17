fc4188e8856eced088fe5c2e896ba27a23fa37ab
Justin Humm <justin.humm@posteo.de>
2018-02-17 23:29:39 +0100

    batman: gebe gw Adresse

--- batman/default.nix
+++ batman/default.nix
@@ -21,11 +21,20 @@ mkIf cfg.enable {
       wantedBy = [ "multi-user.target" ];
       script = ''
         # TODO enp4s5 kann später z.B. durch cfg.network.lan."ff-local".interface ersetzt werden 
-        ${pkgs.batctl}/bin/batctl -m "bat-lan" if add enp4s5
+        ${pkgs.batctl}/bin/batctl -m "bat-lan" interface add enp4s5
+        ${pkgs.batctl}/bin/batctl -m "bat-lan" gw_mode server
       '';
       serviceConfig.Type = "oneshot";
     };
+    
     # "batman-wan" = {
     # };
   };
+
+  networking.interfaces."bat-lan" = {
+    ipAddress = "10.190.135.1";
+    prefixLength = 21;
+  };
+
+  
 }
