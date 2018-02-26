2fde04edaa734872e01d43ba8a41aed5d271fb9d
Justin Humm <justin.humm@posteo.de>
2018-02-26 03:14:01 +0100

    introduce release.nix for prebuilding images. Added a Futro example

---
+++ release.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+
+{
+
+  # This is far from working, but should build an image
+  "futro-S550" = import <nixpkgs/nixos/lib/make-disk-image.nix> {
+    name = "futro-S550-freifunk-gateway";
+    inherit pkgs;
+    inherit (pkgs) lib;
+    config = (import <nixpkgs/nixos/lib/eval-config.nix> {
+      modules = [ 
+        # This is where my config lives
+        ./deploy/configuration.nix 
+      ];
+      # Cross building fails atm...
+      # system = "i686-linux";
+    }).config;
+    # Can we bring this down to << 1024?
+    diskSize = 2048;
+    installBootLoader = false;
+  };
+
+}
